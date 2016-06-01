82fdc8f5fa93a234742aa831545fbe3ae2247019
frizz925 <frizz925@hotmail.com>
2016-06-01 07:02:44 +0700

    a

---
+++ src/index.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+render(<h3>Hello, world!</h3>, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    entry: [
+        'webpack-dev-server/client?http://127.0.0.1:3000',
+        'webpack/hot/only-dev-server',
+        __dirname + '/src/index.jsx'
+    ],
+
+    output: {
+        path: path.join(__dirname, 'public/js'),
+        filename: "bundle.js",
+        publicPath: "/static/"
+    },
+
+    module: {
+        loaders: [{
+            test: /\.js(x)?$/,
+            loaders: ['react-hot', 'babel'],
+            include: path.join(__dirname, 'src')
+        }]
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+};
